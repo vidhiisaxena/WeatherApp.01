91a1ea2770c7b2ad507cd64ebe0c57c4f391d7d2
Vidhi Saxena <vidhi094btmae23@igdtuw.ac.in>
2024-05-18 23:09:53 +0530

    added axios and weather api the website

--- work/src.js
+++ work/src.js
@@ -1,14 +1,22 @@
+function showTemp(response) {
+  let temperature = document.querySelector(".current-temperature-value");
+  temperature.innerHTML = response.data.temperature.current;
+}
+
 function search(event) {
   event.preventDefault();
   let searchInputElement = document.querySelector("#search-input").value;
   let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchInputElement;
+  cityElement.innerHTML = searchInputElement.value;
+  let api = "057314561f8344abb8d5d80t6761o6ae";
+  let city = searchInputElement.value;
+  let apiKey = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${api}&units=metric`;
+  axios.get(apiKey).then(showTemp);
 }
 
 let forms = document.querySelector("#Search-form");
 forms.addEventListener("submit", search);
 
-//task2
 let time = document.querySelector("#current-time");
 let days = [
   "Sunday",
@@ -23,4 +31,3 @@ let day = days[new Date().getDay()];
 let hour = new Date().getHours();
 let minute = new Date().getMinutes();
 time.innerHTML = `${day} ${hour}:${minute}`;
-
