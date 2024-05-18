cf86f3e38bbd0e92c6ac92dea7bd3a86e39be366
Vidhi Saxena <vidhi094btmae23@igdtuw.ac.in>
2024-05-18 23:13:48 +0530

    Update src.js

--- work/src.js
+++ work/src.js
@@ -1,15 +1,15 @@
 function showTemp(response) {
   let temperature = document.querySelector(".current-temperature-value");
-  temperature.innerHTML = response.data.temperature.current;
+  temperature.innerHTML = Math.round(response.data.temperature.current);
 }
 
 function search(event) {
   event.preventDefault();
   let searchInputElement = document.querySelector("#search-input").value;
   let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchInputElement.value;
+  cityElement.innerHTML = searchInputElement;
   let api = "057314561f8344abb8d5d80t6761o6ae";
-  let city = searchInputElement.value;
+  let city = searchInputElement;
   let apiKey = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${api}&units=metric`;
   axios.get(apiKey).then(showTemp);
 }
